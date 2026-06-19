30f9313f444f2a4ccaabf2d2f9e01758aafebef0
Prakash <prakashyarragunta@gmail.com>
2026-06-19 23:07:28 +0530

    leetcode potd FindHighestAlititude

---
+++ POTD/2026/june/FindHighestAltitude19.java
@@ -0,0 +1,26 @@
+/*
+Platform : LeetCode
+Problem  : Find Duplicate Number
+Difficulty : Medium
+Topic : Array, Two Pointers
+Date : 19-06-2026
+
+TC : O(n)
+SC : O(1)
+ */
+
+import java.util.*;
+
+public class FindHighestAltitude19 {
+
+    public int largestAltitude(int[] gain) {
+        int n = gain.length;
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + gain[i - 1];
+        }
+        int res = Arrays.stream(arr).max().orElse(-1);
+        return res;
+    }
+}
